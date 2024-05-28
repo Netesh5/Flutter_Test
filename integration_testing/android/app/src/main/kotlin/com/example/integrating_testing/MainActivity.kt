1722be33e04a280dbf5406d5234460ff449e0598
Netesh5 <netesh.paudel@gmail.com>
2024-05-28 11:23:53 +0545

    setup for integration_test

---
+++ integration_testing/android/app/src/main/kotlin/com/example/integrating_testing/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.integrating_testing
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ integration_testing/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:integrating_testing/screens/login_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Form Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const LoginPage(),
+    );
+  }
+}
